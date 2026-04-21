da1e908dbfe76272bd0951a12200a2c558bffbd9
MarcinStolarczyk01u8 <marcin.stolarczyk@unit8.co>
2026-04-21 18:39:14 +0200

    fix running script

--- run.sh
+++ run.sh
@@ -8,7 +8,12 @@ cd "$SCRIPT_DIR"
 sudo apt-get update
 sudo apt-get install -y stress
 
+# Install pip
+sudo apt install -y python3-pip
 
-pip install -q -r requirements.txt
+# Create venv
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install -q -r requirements.txt
 
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
